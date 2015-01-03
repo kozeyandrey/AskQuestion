1d4b58110d33706dad25113f5d3bd6001aa1da88
kozeyandrey <kozeyandrey@gmail.com>
2015-01-03 16:27:47 +0200

    Create Question and Response entities

---
+++ src/AppBundle/Entity/Question.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="question")
+ */
+class Question
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="text")
+     */
+    protected $description;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $tags;
+    /**
+     * @ORM\OneToMany(targetEntity="response", mappedBy="question")
+     */
+    protected $response;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Question
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Question
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set tags
+     *
+     * @param string $tags
+     * @return Question
+     */
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    /**
+     * Get tags
+     *
+     * @return string 
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->response = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add response
+     *
+     * @param \AppBundle\Entity\response $response
+     * @return Question
+     */
+    public function addResponse(\AppBundle\Entity\response $response)
+    {
+        $this->response[] = $response;
+
+        return $this;
+    }
+
+    /**
+     * Remove response
+     *
+     * @param \AppBundle\Entity\response $response
+     */
+    public function removeResponse(\AppBundle\Entity\response $response)
+    {
+        $this->response->removeElement($response);
+    }
+
+    /**
+     * Get response
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
---
+++ src/AppBundle/Entity/Response.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="response")
+ */
+class Response
+{
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="text")
+     */
+    protected $description;
+    /**
+     * @var string
+     * @ORM\ManyToOne(targetEntity="Question", inversedBy="response",cascade={"persist"})
+     * @ORM\JoinColumn(name="question", referencedColumnName="id")
+     */
+    protected $question;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Response
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set question
+     *
+     * @param \AppBundle\Entity\Question $question
+     * @return Response
+     */
+    public function setQuestion(\AppBundle\Entity\Question $question = null)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return \AppBundle\Entity\Question 
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+}
