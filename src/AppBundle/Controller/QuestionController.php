60dedb4c41d3431dc28f7158abe4141b609253de
kozeyandrey <kozeyandrey@gmail.com>
2015-01-05 03:05:27 +0200

    Create QuestionController

--- src/AppBundle/Controller/QuestionController.php
+++ src/AppBundle/Controller/QuestionController.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 04.01.15
- * Time: 0:22
- */ 
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\Type\AskQuestionType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Question;
+use Symfony\Component\HttpFoundation\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * This method show the form for ask question
+     *
+     * @param Request $request
+     * @return array
+     *
+     * @Template()
+     */
+    public function askAction(Request $request)
+    {
+        $question = new Question();
+        $form = $this->createForm(new AskQuestionType(), $question);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            return $this->redirect($this->generateUrl('home'));
+            }
+        return $this->render('AppBundle:AskQuestion:ask.html.twig', array(
+            'form' => $form->createView()));
+
+//        return [
+//            'questions'=>$questions
+//        ];
+    }
+}
