599521674bd43d3522f78a972f0ce460075eb293
kozeyandrey <kozeyandrey@gmail.com>
2015-01-08 00:45:01 +0200

    Add show all tags

---
+++ src/AppBundle/Controller/TagController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Question;
+use Symfony\Component\HttpFoundation\Request;
+
+class TagController extends Controller
+{
+    /**
+     * This method render all questions
+     *
+     * @param Request $request
+     * @return array
+     *
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        $tags = $this->getDoctrine()->getManager()->getRepository("AppBundle:Tag")->findAll();
+        $paginator  = $this->get('knp_paginator');
+        $questions = $paginator->paginate(
+            $tags,
+            $request->query->get('page', 1),
+            10
+        );
+
+        return [
+            'tags'=>$tags
+        ];
+    }
+}
